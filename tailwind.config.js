cc3fbf3f151f194fb9bad1f5d51603858b0fde1f
fareedjaved203 <fareedjaved203@gmail.com>
2024-03-07 15:09:59 +0500

    login and not found pages created

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
